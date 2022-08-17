21df542e66d41114866aa508f45fb66f0a245e33
Conrad Russo <conradr@gmail.com>
2022-08-17 21:13:54 +0100

    authors front end

--- controllers/author_controller.py
+++ controllers/author_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from repositories import author_repository as ar
+from models.author_class import Author
 
 book_blueprint = Blueprint("books", __name__)
 
@@ -8,3 +9,33 @@ book_blueprint = Blueprint("books", __name__)
 def all_authors():
     authors = ar.show_all_authors()
     return render_template('authors/authors_index.html', authors=authors)
+
+
+@book_blueprint.route('/authors', methods=['POST'])
+def new_author():
+    author_name = request.form['author_name']
+    author = Author(author_name)
+    ar.save_author(author)
+    authors = ar.show_all_authors()
+    return render_template('authors/authors_index.html', authors=authors)
+
+
+@book_blueprint.route('/authors/<id>', methods=['POST'])
+def save_updated_author(id):
+    author_name = request.form['author_name']
+    author = Author(author_name)
+    ar.update_author_by_id(author)
+    return render_template('authors/authors_index.html', authors=authors)
+
+
+@book_blueprint.route('/authors/<id>/update', methods=['GET'])
+def update_author(id):
+    author = ar.select_author_by_id(id)
+    ar.update_author_by_id(author)
+    return render_template('authors/authors_edit.html', author=author)
+
+
+@book_blueprint.route('/authors/new')
+def new_task():
+    author_list = ar.show_all_authors()
+    return render_template('authors/authors_new.html')
